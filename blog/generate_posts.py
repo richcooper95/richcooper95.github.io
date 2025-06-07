6c5d2be2e810c9d28d9338fd2c3fe940cbc4e9ad
Rich Barton-Cooper <richwjcooper@gmail.com>
2025-06-07 23:34:37 +0100

    Fix blog icon and favicons

--- blog/generate_posts.py
+++ blog/generate_posts.py
@@ -81,7 +81,7 @@ def generate_post_html(metadata, content, slug):
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="/static/style.css">
     <link rel="stylesheet" href="/blog/blog.css">
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>">
+    <link rel="icon" href="/static/images/pencil-icon.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet">
